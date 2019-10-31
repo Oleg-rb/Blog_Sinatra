405237634a61399195289b65b184eee869c199ab
Oleg-rb <oleg1107@rambler.ru>
2019-10-31 22:02:05 +0300

    Save comment to database

--- Blog.rb
+++ Blog.rb
@@ -67,5 +67,18 @@ post '/details/:post_id' do
 	post_id = params[:post_id]
 	content = params[:content]
 
-  erb "You typed comment #{content} for post #{post_id}"
+	@db.execute 'insert into Comments 
+		(
+			content, 
+			created_date, 
+			post_id
+		) 
+			values 
+		(
+			?, 
+			datetime(), 
+			?
+		)', [content, post_id]
+
+  erb redirect('/details/' + post_id)
 end
