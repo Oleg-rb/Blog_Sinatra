07e63c34071b62842e09d9de6904b8924e1d7d25
Oleg-rb <oleg1107@rambler.ru>
2019-10-31 14:21:12 +0300

    Init database, make @db

--- Blog.rb
+++ Blog.rb
@@ -4,6 +4,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+before do
+	init_db
+end
+
+def init_db
+	@db = SQLite3::Database.new 'blog.db'
+	@db.results_as_hash = true
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
 end
