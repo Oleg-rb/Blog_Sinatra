3bd3c54f1983c19000d98878053f47c904d2758c
Oleg-rb <oleg1107@rambler.ru>
2019-10-31 13:28:31 +0300

    Add stub for /new

--- Blog.rb
+++ Blog.rb
@@ -7,3 +7,7 @@ require 'sqlite3'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
 end
+
+get '/new' do
+  erb "Hello World"
+end
